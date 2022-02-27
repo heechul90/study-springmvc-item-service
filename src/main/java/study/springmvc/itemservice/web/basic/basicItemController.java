ad0714ea552f5288f9923f307b2d79f7a7e185d4
hclee <heechul4296@gmail.com>
2022-02-27 22:45:51 +0900

    RedirectAttributes

--- src/main/java/study/springmvc/itemservice/web/basic/basicItemController.java
+++ src/main/java/study/springmvc/itemservice/web/basic/basicItemController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import study.springmvc.itemservice.domain.item.Item;
 import study.springmvc.itemservice.domain.item.ItemRepository;
 
@@ -116,10 +117,24 @@ public class basicItemController {
      * @param item
      * @return
      */
-    @PostMapping(value = "/add")
+    //@PostMapping(value = "/add")
     public String addItemV5(@ModelAttribute("item") Item item) {
         itemRepository.save(item);
         return "redirect:/basic/items/" + item.getId();
+    }
+
+    /**
+     * 상품 등록 v6
+     * @param item
+     * @return
+     */
+    @PostMapping(value = "/add")
+    public String addItemV6(@ModelAttribute("item") Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
 
