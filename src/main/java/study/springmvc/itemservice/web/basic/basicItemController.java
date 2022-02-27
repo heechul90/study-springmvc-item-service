f65c1430acd1e23cace54d3890be4fc42912be22
hclee <heechul4296@gmail.com>
2022-02-27 21:27:20 +0900

    상품 상세

--- src/main/java/study/springmvc/itemservice/web/basic/basicItemController.java
+++ src/main/java/study/springmvc/itemservice/web/basic/basicItemController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import study.springmvc.itemservice.domain.item.Item;
 import study.springmvc.itemservice.domain.item.ItemRepository;
@@ -19,11 +20,29 @@ public class basicItemController {
 
     private final ItemRepository itemRepository;
 
+    /**
+     * 상품 목록
+     * @param model
+     * @return
+     */
     @GetMapping
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
+    }
+
+    /**
+     * 상품 상세
+     * @param itemId
+     * @param model
+     * @return
+     */
+    @GetMapping(value = "/{itemId}")
+    public String item(@PathVariable("itemId") Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
     }
 
     /**
